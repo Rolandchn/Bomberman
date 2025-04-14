6268e73776cefbb6a612153d7e6be312f0e3c566
Lylichen07 <feedpoipoi@gmail.com>
2025-04-14 14:00:08 +0200

    add comments

--- data/entity/IA.py
+++ data/entity/IA.py
@@ -14,6 +14,9 @@ from data.texture.config import TILE_SIZE
 
 
 class IA(Entity):
+    # difficulty:
+    #   easy: random
+    #   moderate:  follow td4 (strategie: take the center, destroy obstacle, corner the player, ...)
     def __init__(self, spawn:Tuple[int, int], entities:EntityManager):
         super().__init__(spawn, pygame.Surface((TILE_SIZE, TILE_SIZE)), entities.player_group)
 
