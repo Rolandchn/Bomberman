a07ae679a6bd4f91fbabe137716306bf6381b627
Lylichen07 <feedpoipoi@gmail.com>
2025-04-14 13:36:05 +0200

    add IA structure

---
+++ data/entity/IA.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Tuple
+
+if TYPE_CHECKING:
+    from data.entity.EntityManager import EntityManager
+
+
+import pygame
+
+from data.entity.Entity import Entity
+
+from data.texture.Color import Color
+from data.texture.config import TILE_SIZE
+
+
+class IA(Entity):
+    def __init__(self, spawn:Tuple[int, int], entities:EntityManager):
+        super().__init__(spawn, pygame.Surface((TILE_SIZE, TILE_SIZE)), entities.player_group)
+
+        self.life = 1
+
+        self.entities = entities
+
+        self.image.fill(Color.BLACK.value)
