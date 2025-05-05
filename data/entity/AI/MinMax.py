f5f05967a1661498eebe83e1776114ea6985be0a
malik439 <Mlkdf.7518@gmail.com>
2025-05-05 19:10:28 +0200

    "changement MinMax"

--- data/entity/AI/MinMax.py
+++ data/entity/AI/MinMax.py
@@ -14,48 +14,38 @@ from data.entity.AI.Result import result
 from data.entity.AI.Evaluation import eval
 from data.entity.AI.Terminal import terminal
 
-def minmax(simulated_world: GameWorld, root_entity:GameStatus, depth=3, alpha=float('-inf'), beta=float('inf')):
+
+def minmax(simulated_world: GameWorld, root_entity: GameStatus, depth=3):
     player: Entity = turn(simulated_world)
-    
+
+
     if terminal(simulated_world) or depth <= 0:
         return eval(simulated_world, root_entity), None
-    
+
     # MIN
     if player.status == GameStatus.P1:
         best_value = float("inf")
-        best_action = None
+        best_action=None
 
         for action in actions(simulated_world, player):
-            value, _ = minmax(result(simulated_world,  action), root_entity, depth - 1, alpha, beta)
+            value, _ = minmax(result(simulated_world, action), root_entity, depth - 1)
 
-            if value < best_value:
+            if value <= best_value:
                 best_value = value
                 best_action = action
 
-            beta = min(beta, best_value)
-
-            #si beta <= alpha : on arrête d'explorer
-            if beta <= alpha:
-                break
-
         return best_value, best_action
-        
+
     # MAX
     elif player.status == GameStatus.P2:
         best_value = float("-inf")
         best_action = None
 
         for action in actions(simulated_world, player):
-            value, _ = minmax(result(simulated_world, action), root_entity, depth - 1, alpha, beta)
+            value, _ = minmax(result(simulated_world, action), root_entity, depth - 1)
 
-            if best_value > value:
+            if best_value <= value:
                 best_value = value
                 best_action = action
 
-            alpha = max(alpha, best_value)
-
-            #si beta <= alpha -> on arrête d'explorer
-            if beta <= alpha:
-                break
-            
         return best_value, best_action
